fcc11e22271b7e44f3ad318db53e129a01df1762
christopher and kyle <foo@bar.com>
2013-01-30 15:08:38 -0800

    blogger proggress

---
+++ blogger/app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :title, :body, :author_name
+
+  belongs_to :article
+end
---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'f7a84b2d730b87e9094e95c8083e4c126bdb7fff98345b17a327789bed86e47989aa4eebf506d1d2777f8239fd78cc4eb2b1c96d5e10579a6bafc2f9c9b2e0b1'
